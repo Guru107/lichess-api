dae70978411ac024374e0af5ddc0a9d84a7c4bc2
Guru107 <guruak107@gmail.com>
2024-07-07 21:50:42 +0530

    fix import

--- app/app.py
+++ app/app.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from aggregations.player_aggragation import PlayerAggregation
-import json
+from .aggregations import player_aggragation
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(debug=True)
-player_agg = PlayerAggregation()
+player_agg = player_aggragation.PlayerAggregation()
 
 @app.get("/")
 async def root():
